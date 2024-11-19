3df772f456499a993418d320d758e7cf5d9ff404
Aaron Hossain <2071222@edu.vaniercollege.qc.ca>
2024-11-19 09:42:10 -0500

    Exam Submission

--- starter-final/CannonBall.java
+++ starter-final/CannonBall.java
@@ -14,5 +14,45 @@ public class CannonBall extends Actor
      */
     public void act()
     {
+        if (isGameLost()) {
+            transitionToGameOverWorld();
+        }
+        kill();
+    }
+
+    /**
+     * When a cannon ball hits the ladybug, game is lost.
+     */
+    public boolean isGameLost()
+    {
+        World world = getWorld();
+        if (world.getObjects(Ladybug.class).isEmpty()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /**
+     * Transitions to Game Over Screen with a losing sound.
+     */
+    public void transitionToGameOverWorld()
+    {
+        World GameOver =  new GameOver();
+        Greenfoot.setWorld(GameOver);
+        Greenfoot.playSound("lose.wav");
+    }
+
+    /**
+     * If a cannonball collides with a ladybug, it kills it. 
+     */
+    public void kill()
+    {
+        Actor ladybug = getOneIntersectingObject(Ladybug.class);
+        if (ladybug != null) {
+            World world = getWorld();
+            world.removeObject(ladybug);
+        }
     }
 }
--- starter-final/Canon.java
+++ starter-final/Canon.java
@@ -8,11 +8,21 @@ import greenfoot.*;
  */
 public class Canon extends Actor
 {
+    private static final double CANNON_BALL_VELOCITY = 1500.0;
 
     /**
      * Act - do whatever the Canon wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
+    }
+
+    /**
+     * 
+     */
+    public void cannonShoot()
+    {
+        CannonBall newBall =  new CannonBall();
+        getWorld().addObject(newBall, getX(), getY());
     }
 }
